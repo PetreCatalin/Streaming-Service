e8d4f3fb6b33ae7f09a55fe9da9401e4151b750e
Catalin Petre <Catalin.Petre@interactivesw.ro>
2019-06-03 13:53:10 +0300

    chaotic image crypto

---
+++ Disertatie/src/app/utils/ElementRenderer.ts
@@ -0,0 +1,7 @@
+import { PwlcmSpicCipher3 } from 'chaotic-image-crypto/lib/bindings';
+import { RgbUtils } from './RgbUtils';
+import base64js  from 'base64-js'; //https://www.npmjs.com/package/base64-js
+
+export class ElementRenderer {
+    
+}
